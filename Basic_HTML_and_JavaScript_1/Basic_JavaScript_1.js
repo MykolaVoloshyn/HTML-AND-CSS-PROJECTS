d26175c91cb5f60d8e262512721587978e26b05b
MykolaVoloshyn <mykolavoloshyn123@gmail.com>
2025-03-13 20:44:47 -0700

    Executed exercises using js and html

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,7 +1,18 @@
 alert("Hello, World! I am an alert box!");
 
 function MyFirstFunction() {
-  var String = "Kiss me, I'm Irish!";
-  var result = String.fontcolor("green");
-  document.getElementById("Irish").innerHTML = result;
+    var String = "Kiss me, I'm Irish!";
+    var result = String.fontcolor("green");
+    document.getElementById("Irish").innerHTML = result;
 }
+
+function changeTextSize() {
+    const elementsWithClassParagraph = document.getElementsByClassName("paragraph");
+    elementsWithClassParagraph[0].style.fontSize = "xx-large";
+    elementsWithClassParagraph[1].style.fontSize = "large";
+}
+
+var canvas = document.getElementById("myCanvas");
+var canvasText = canvas.getContext("2d");
+canvasText.font = "30px Arial";
+canvasText.fillText("Hello World", 20, 60);
